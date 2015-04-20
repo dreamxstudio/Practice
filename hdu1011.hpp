99716449595f4539afdc32d6f726f54d02158f7b
dreamxstudio <dreamware@outlook.com>
2015-04-20 17:19:25 +0800

    1011

--- hdu1011.hpp
+++ hdu1011.hpp
@@ -28,12 +28,42 @@
 
 BEGIN(hdu1011)
 
+const int MAXN = 100;
+
+struct Node
+{
+	int from;
+	int to;
+	int bug;
+	int brain;
+};
+
+Node nodes[MAXN+1];
+int dp[MAXN+1][MAXN+1];
+
+void tree_dp(Node root)
+{
+
+}
+
 void run()
 {
 	int N, M;
 	for (;scanf("%d%d",&N,&M),N!=-1&&M!=-1;)
 	{
+		for (int i = 0; i < N; ++i)
+			scanf("%d%d", &nodes[i].bug, &nodes[i].brain);
 
+		int from, to;
+
+		for (int i = 0; i < M; ++i)
+		{
+			scanf("%d %d", &from, &to);
+			nodes[from].to = to;
+			nodes[to].from = from;
+		}
+
+		tree_dp(nodes[0]);
 	}
 }
 
